90d340a636fffa0999e2f8e8576773490c1f061e
LivBukowska <liv.bukowska@gmail.com>
2022-04-23 14:19:01 +0200

    Add tests for wind vector components

--- meteogram/tests/test_meteogram.py
+++ meteogram/tests/test_meteogram.py
@@ -1,7 +1,8 @@
 """Test use of the meteogram module."""
+import datetime
 
 from meteogram import meteogram
-
+from numpy.testing import assert_almost_equal
 
 #
 # Example starter test
@@ -26,6 +27,20 @@ def test_degF_to_degC_at_freezing():
 # Instructor led introductory examples
 #
 
+def test_title_case():
+    # Setup
+    in_string = 'this is a test string'
+    desired = 'This Is A Test String'
+
+    # Exercise
+    actual = in_string.title()
+
+    # Verify
+    assert actual == desired
+
+    # Cleanup - none necessary
+
+
 #
 # Instructor led examples of numerical comparison
 #
@@ -34,25 +49,106 @@ def test_degF_to_degC_at_freezing():
 # Exercise 1
 #
 def test_build_asos_request_url_single_digit_datetimes():
-    """
-    Test building URL with single digit month and day.
-    """
+    # Setup
+    start = datetime.datetime(2021, 1, 1, 1)
+    end = datetime.datetime(2021, 1, 5, 1)
+    station = 'FSD'
+
+    # Exercise
+    result_url = meteogram.build_asos_request_url(station, start, end)
+
+    # Verify
+    truth_url = 'https://mesonet.agron.iastate.edu/request/asos/1min_dl.php?station%5B%5D=FSD&tz=UTC&year1=2021&month1=01&day1=01&hour1=01&minute1=00&year2=2021&month2=01&day2=05&hour2=01&minute2=00&vars%5B%5D=tmpf&vars%5B%5D=dwpf&vars%5B%5D=sknt&vars%5B%5D=drct&sample=1min&what=view&delim=comma&gis=yes'
+    assert result_url == truth_url
+
+    # Cleanup - none
+
     pass
 
 
 def test_build_asos_request_url_double_digit_datetimes():
-    """
-    Test building URL with double digit month and day.
-    """
+    # Setup
+    start = datetime.datetime(2021, 11, 11, 11)
+    end = datetime.datetime(2021, 11, 15, 11)
+    station = 'FSD'
+
+    # Exercise
+    result_url = meteogram.build_asos_request_url(station, start, end)
+
+    # Verify
+    truth_url = 'https://mesonet.agron.iastate.edu/request/asos/1min_dl.php?station%5B%5D=FSD&tz=UTC&year1=2021&month1=11&day1=11&hour1=11&minute1=00&year2=2021&month2=11&day2=15&hour2=11&minute2=00&vars%5B%5D=tmpf&vars%5B%5D=dwpf&vars%5B%5D=sknt&vars%5B%5D=drct&sample=1min&what=view&delim=comma&gis=yes'
+    assert result_url == truth_url
+
+    # Cleanup - none
+
     pass
 
 #
 # Exercise 1 - Stop Here
 #
 
+def test_does_three_equal_three():
+    assert 3 == 3
+
+def test_floating_substraction():
+    # Setup
+    desired = 0.294
+
+    # Exercise
+    actual = 1 - 0.706
+
+    # Verify
+    # assert abs(actual-desired) < 0.00001
+    assert_almost_equal(actual, desired, 6)
+
+    # Cleanup - none
+
 #
-# Exercise 2 - Add calculation tests here
-#
+def test_vector_components_of_wind_direction_0_degree():
+    # Setup
+    desired = 0, -100
+    # Exercise
+    actual = meteogram.wind_components(100, 0)
+
+    # Verify
+    assert_almost_equal(actual, desired, 6)
+    # Cleanup
+
+def test_vector_components_of_wind_direction_45_degree():
+    # Setup
+    desired = -85.090352, -52.532198
+
+    # Exercise
+    actual = meteogram.wind_components(100, 45)
+
+    # Verify
+    assert_almost_equal(actual, desired, 6)
+
+    # Cleanup
+
+def test_vector_components_of_wind_direction_360_degree():
+    # Setup
+    desired = -95.891572, 28.369109
+
+    # Exercise
+    actual = meteogram.wind_components(100, 360)
+
+    # Verify
+    assert_almost_equal(actual, desired, 6)
+
+    # Cleanup
+
+def test_vector_components_of_wind_direction_0_speed():
+    # Setup
+    desired = 0, 0
+
+    # Exercise
+    actual = meteogram.wind_components(0, 45)
+
+    # Verify
+    assert_almost_equal(actual, desired)
+
+    # Cleanup
 
 #
 # Exercise 2 - Stop Here
